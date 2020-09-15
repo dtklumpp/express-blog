175a086561f79caf8883b97702ae9a9ad02c56d2
DaltonHart <daltonvaren@gmail.com>
2020-09-15 12:33:53 -0700

    adds author index route

--- server.js
+++ server.js
@@ -13,10 +13,30 @@ const PORT = 4000;
 app.set("view engine", "ejs");
 
 /* Routes */
+
+// view routes
 app.get("/", function (req, res) {
   // render("file", context)
   res.render("index");
 });
+
+// Author Routes
+
+// index view
+app.get("/authors", function (req, res) {
+  // mongoose code
+  db.Author.find({}, function (error, foundAuthors) {
+    if (error) return res.send(error);
+
+    const context = {
+      authors: foundAuthors,
+    };
+
+    res.render("author/index", context);
+  });
+});
+
+// Article Routes
 
 /* Server Listener */
 app.listen(PORT, function () {
