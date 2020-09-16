2a77f50e614abfbced3b17bf21827c7199bb4829
DaltonHart <daltonvaren@gmail.com>
2020-09-16 10:21:56 -0700

    refactors models to add relationship one to many

--- models/Article.js
+++ models/Article.js
@@ -4,6 +4,7 @@ const articleSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     body: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "Author" },
   },
   {
     timestamps: true,
--- models/Author.js
+++ models/Author.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 const authorSchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "You must provide a name."] },
+    // example articles: ["asd90f87asdf09asd"]
+    articles: [
+      // reference
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Article",
+      },
+    ],
   },
   {
     timestamps: true, // adds a createdAt and an updatedAt
